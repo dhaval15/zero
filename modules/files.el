f48876ae838303e700f15bd79b6805c3f4ef5917
Dhaval Patel <dhavalpatel32768@gmail.com>
2022-08-20 00:48:08 -0400

    fixed tripple keystrokes

--- modules/files.el
+++ modules/files.el
@@ -3,5 +3,5 @@
   (counsel-find-file zero/org-dir))
 
 (nvmap :states '(normal visual) :keymaps 'override :prefix "SPC f" :which-key "Quick Access"
-       "f h" '(counsel-find-file-at-home :which-key "Find files")
-       "f f" '(counsel-find-file :which-key "Find files"))
+       "h" '(counsel-find-file-at-home :which-key "Find files")
+       "f" '(counsel-find-file :which-key "Find files"))
